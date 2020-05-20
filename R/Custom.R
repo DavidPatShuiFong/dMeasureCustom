353e097cc44051d23cfed0aab28867c1bd83173c
David Fong <vkelim@bigpond.com>
2020-05-20 22:31:25 +1000

    storage of patient lists in dMeasure
    * initialize_data_table to create table CustomPatientLists in
       dMeasure's SQLite configuration database
    * read_configuration_db reads CustomPatientLists and stores
       the list/dataframe in self$patientList
      columns are id (numeric), name (character) and patientList (blob)
      note that $patientList cannot be manipulated by dplyr due
      to the presence of the blob.
    * write_patientList - adds patient list stored in CSV
    * remove_patientList - removes by name

--- R/Custom.R
+++ R/Custom.R
@@ -12,63 +12,64 @@ NULL
 #' @title dMeasureCustom class
 #' @description list appointments (custom) by clinician providers
 #' @export
-dMeasureCustom <- R6::R6Class("dMeasureCustom",
-                              public = list(
-                                # dM is a dMeasure object
-                                dM = NULL,
-                                patient_list = NULL,
-                                initialize = function (dMeasure_obj) {
-                                  # dMeasure_obj is a R6 dMeasure object
-                                  self$dM <- dMeasure_obj
-                                  self$patient_list <- read.csv(system.file("202003AsthmaCOPD_Phase1.csv",
-                                                                            package = "dMeasureCustom"),
-                                                                stringsAsFactors = FALSE)
-                                  if (length(public_init_fields$name) > 0) { # only if any defined
-                                    for (i in 1:length(public_init_fields$name)) {
-                                      if (public_init_fields$obj[[i]] == "dMeasureCustom") {
-                                        self[[public_init_fields$name[[i]]]] <-
-                                          eval(public_init_fields$value[[i]]) # could 'quote' the value
-                                      }
-                                    }
-                                  }
-                                  if (length(private_init_fields$name) > 0) { # only if any defined
-                                    for (i in 1:length(private_init_fields$name)) {
-                                      if (private_init_fields$obj[[i]] == "dMeasureCustom") {
-                                        private[[private_init_fields$name[[i]]]] <-
-                                          eval(private_init_fields$value[[i]]) # could 'quote' the value
-                                      }
-                                    }
-                                  }
+dMeasureCustom <- R6::R6Class(
+  "dMeasureCustom",
+  public = list(
+    # dM is a dMeasure object
+    dM = NULL, # pointer to dMeasure R6 object
+    patientLists = NULL,
+    # pointer to patientLists table in configuration database
+    patientList = NULL,
+    # the actual patient lists
+    initialize = function (dMeasure_obj) {
+      # dMeasure_obj is a R6 dMeasure object
+      self$dM <- dMeasure_obj
 
-                                  if (requireNamespace("shiny", quietly = TRUE)) {
-                                    # set reactive version only if shiny is available
-                                    # note that this is for reading (from programs calling this object) only!
-                                    if (length(reactive_fields$name) > 0) { # only if any .reactive() defined
-                                      for (i in 1:length(reactive_fields$name)) {
-                                        if (reactive_fields$obj[[i]] == "dMeasureCustom") {
-                                          self[[reactive_fields$name[[i]]]] <- shiny::reactiveVal(
-                                            eval(reactive_fields$value[[i]]) # could 'quote' the value
-                                          )
-                                        }
-                                      }
-                                    }
-                                    if (length(reactive_event$name) > 0) { # only if any .reactive() defined
-                                      for (i in 1:length(reactive_event$name)) {
-                                        if (reactive_event$obj[[i]] == "dMeasureCustom") {
-                                          self[[reactive_event$name[[i]]]] <-
-                                            eval(reactive_event$value[[i]]) # could 'quote' the value
-                                        }
-                                      }
-                                    }
-                                  }
-                                }
-                              )
-                              # this is a 'skeleton' class
-                              # it is filled in the with the '.public' function
+      if (length(public_init_fields$name) > 0) { # only if any defined
+        for (i in 1:length(public_init_fields$name)) {
+          if (public_init_fields$obj[[i]] == "dMeasureCustom") {
+            self[[public_init_fields$name[[i]]]] <-
+              eval(public_init_fields$value[[i]]) # could 'quote' the value
+          }
+        }
+      }
+      if (length(private_init_fields$name) > 0) { # only if any defined
+        for (i in 1:length(private_init_fields$name)) {
+          if (private_init_fields$obj[[i]] == "dMeasureCustom") {
+            private[[private_init_fields$name[[i]]]] <-
+              eval(private_init_fields$value[[i]]) # could 'quote' the value
+          }
+        }
+      }
+
+      if (requireNamespace("shiny", quietly = TRUE)) {
+        # set reactive version only if shiny is available
+        # note that this is for reading (from programs calling this object) only!
+        if (length(reactive_fields$name) > 0) { # only if any .reactive() defined
+          for (i in 1:length(reactive_fields$name)) {
+            if (reactive_fields$obj[[i]] == "dMeasureCustom") {
+              self[[reactive_fields$name[[i]]]] <- shiny::reactiveVal(
+                eval(reactive_fields$value[[i]]) # could 'quote' the value
+              )
+            }
+          }
+        }
+        if (length(reactive_event$name) > 0) { # only if any .reactive() defined
+          for (i in 1:length(reactive_event$name)) {
+            if (reactive_event$obj[[i]] == "dMeasureCustom") {
+              self[[reactive_event$name[[i]]]] <-
+                eval(reactive_event$value[[i]]) # could 'quote' the value
+            }
+          }
+        }
+      }
+    }
+  )
+  # this is a 'skeleton' class
+  # it is filled in the with the '.public' function
 )
 
 ##### special reactive functions ##########################
-
 
 .private(dMeasureCustom, "set_reactive", function(myreactive, value) {
   # reactive (if shiny/reactive environment is available) is set to 'value'
@@ -94,8 +95,10 @@ dMeasureCustom <- R6::R6Class("dMeasureCustom",
 #'
 #' @export
 shinydashboardmenuItem <- function() {
-  return(shinydashboard::menuItem("Custom",
-                                  tabName = "custom", icon = shiny::icon("cocktail")))
+  return(shinydashboard::menuItem(
+    "Custom",
+    tabName = "custom", icon = shiny::icon("cocktail"))
+  )
 }
 
 #' center panel description
@@ -120,6 +123,8 @@ dMeasureShinytabItems <- function() {
 #' Custom module - UI function
 #'
 #' Display appointments within selected range of dates and providers
+#'
+#' @name datatableUI
 #'
 #' @param id module ID (used in conjunction with 'callModule')
 #'
@@ -151,7 +156,7 @@ datatableUI <- function(id) {
 
 #' Custom module - server
 #'
-#' @name custom_dataatable
+#' @name datatableServer
 #'
 #' @param input as required by Shiny modules
 #' @param output as required by Shiny modules
@@ -167,15 +172,19 @@ datatableServer <- function(input, output, session, dMCustom) {
 
   styled_custom_list <- shiny::reactive({
     shiny::validate(
-      shiny::need(dMCustom$dM$appointments_filtered_timeR(),
-                  "No appointments in selected range")
+      shiny::need(
+        dMCustom$dM$appointments_filtered_timeR(),
+        "No appointments in selected range"
+      )
     )
-    datatable_styled(dMCustom$dM$appointments_filtered_timeR() %>>%
-                       dplyr::filter(InternalID %in% dMCustom$patient_list$ID) %>>%
-                       dplyr::left_join(dMCustom$patient_list,
-                                        by = c("InternalID" = "ID")) %>>%
-                       dplyr::select(Patient, AppointmentDate, AppointmentTime,
-                                     Provider, Status, Disease))
+    datatable_styled(
+      dMCustom$dM$appointments_filtered_timeR() %>>%
+        dplyr::filter(InternalID %in% dMCustom$patient_list$ID) %>>%
+        dplyr::left_join(dMCustom$patient_list,
+          by = c("InternalID" = "ID")) %>>%
+        dplyr::select(Patient, AppointmentDate, AppointmentTime,
+          Provider, Status, Label)
+    )
   })
 
   output$custom_table <- DT::renderDT({
@@ -183,3 +192,197 @@ datatableServer <- function(input, output, session, dMCustom) {
   })
 
 }
+
+#' Custom module - initialize database table
+#'
+#' @name initialize_data_table
+#'
+#' @return list
+#'
+#' @export
+initialize_data_table <- function() {
+  return(
+    list(
+      tablename = "CustomPatientLists",
+      variable_list = list(
+        c("id", "integer"),
+        c("Name", "character"),
+        c("patientList", "list")
+        # list will create a 'blob' column
+        # 'raw' doesn't create a 'blob' column!
+      )
+    )
+  )
+}
+
+#' read the configuration database
+#'
+#' @param dMeasureCustom_obj R6 object
+#'
+#' @export
+read_configuration_db <- function(
+  dMeasureCustom_obj) {
+  dMeasureCustom_obj$read_configuration_db()
+}
+.public(dMeasureCustom, "read_configuration_db", function() {
+  # read configuration database for patient lists
+
+  self$patientLists <- self$dM$config_db$conn() %>>%
+    dplyr::tbl("CustomPatientLists")
+  # a link to the table
+
+  self$patientList <-
+    DBI::dbReadTable(self$dM$config_db$conn(), "CustomPatientLists")
+
+  return()
+})
+
+#' write patient list to configuration database
+#'
+#' @param dMeasureCustom_obj R6 object
+#' @param name name of patient list
+#' @param filename CSV (comma-separated-value) filename
+#'   expects at least two columns,
+#'   'ID' and 'Label'
+#' @param column_ID name of ID column
+#' @param column_Label name of label column
+#' @param keepAllColumns if FALSE, trim to just 'ID' and 'Label' columns
+#'
+#' @return ID of created patient list
+#'
+#' @export
+write_patientList <- function(
+  dMeasureCustom_obj,
+  name,
+  filename,
+  column_ID = "ID",
+  column_Label = "Label",
+  keepAllColumns = FALSE) {
+  dMeasureCustom_obj$write_patientList(
+    name, filename,
+    column_ID, column_Label,
+    keepAllColumns
+  )
+}
+.public(dMeasureCustom, "write_patientList",
+  function(
+    name, filename,
+    column_ID = "ID", column_Label = "Label",
+    keepAllColumns = FALSE
+  ) {
+    # write patient list to configuration database
+
+    if (name %in% self$patientLists$Name) {
+      # this name already chosen
+      warning("'", name, "' already exists as a named patient list.")
+      return(NULL)
+    }
+
+    if (!file.exists(filename)) {
+      warning("'", filename, "' does not exist.")
+      return(NULL)
+    }
+
+    read_csv_success <- TRUE # by default
+    tryCatch(
+      d <- read.csv(filename, stringsAsFactors = FALSE),
+      error = function(e) {
+        warning(e)
+        read_csv_success <<- FALSE
+      }
+    )
+    if (!read_csv_success) {
+      warning("Unable to read file '", filename, "' as CSV.")
+      return(NULL)
+    }
+    if (!exists(column_ID, d) || !exists(column_Label, d)) {
+      warning(
+        "File does not contain 'ID' and 'Label' column",
+        " names '", column_ID, "' and '", column_Label, "'."
+      )
+      return(NULL)
+    }
+
+    d <- d %>>%
+      dplyr::mutate(
+        ID = !!as.symbol(column_ID),
+        Label = !!as.symbol(column_Label)
+      ) # converts column names to 'ID' and 'Label'
+    if (!keepAllColumns) {
+      # if keepAllColumns is TRUE, then extra original
+      # columns are kept, which would take more storage space
+      # to store
+      d <- d %>>%
+        dplyr::select(ID, Label)
+    }
+
+    newID <- max(self$patientLists$id, 0) + 1
+    # initially might be an empty set, so need to append a '0'
+    # note that 'id' is the identifier in the configuration database
+    # not the 'ID' column of the patientList!
+
+    query <- paste0(
+      "INSERT INTO CustomPatientLists",
+      "(id, name, patientList)",
+      "VALUES ($id, $name, $patientList)"
+    )
+    data_for_sql <- list(
+      id = newID,
+      name = name,
+      patientList = data.frame(
+        data = I(serialize(d, NULL, xdr = FALSE, version = 3))
+      )
+      # data.frame(data = I(serialize(d))) creates the 'single' object that
+      # dbSendQuery wants
+      # strangely, the data can be read as follow (for the first row)
+      # x <- DBI::dbReadTable(dM$config_db$conn(), "CustomPatientLists")
+      # x$patientList[1] - returns 'blob'
+      # x$patientList[[1]] - returns raw
+      # unserialize(x$patientList[[1]]) - returns dataframe
+      # referring to 'data' as a column seems unnecessary!
+    )
+
+    self$dM$config_db$dbSendQuery(query, data_for_sql)
+
+    return(newID)
+  }
+)
+
+#' remove patient list from configuration database
+#'
+#' @param dMeasureCustom_obj R6 object
+#' @param name name of patient list
+#'
+#' @return TRUE if removed successfully
+#'
+#' @export
+remove_patientList <- function(
+  dMeasureCustom_obj,
+  name) {
+  dMeasureCustom_obj$remove_patientList(
+    name
+  )
+}
+.public(dMeasureCustom, "remove_patientList",
+  function(
+    name
+  ) {
+    # remove patient list from configuration database
+
+    if (!name %in% self$patientList$Name) {
+      # this name already chosen
+      warning("'", name, "' not in patient list.")
+      return(FALSE)
+    }
+
+    query <- paste0(
+      "DELETE FROM CustomPatientLists WHERE name = ?"
+    )
+    data_for_sql <- list(
+      name = name
+    )
+    self$dM$config_db$dbSendQuery(query, data_for_sql)
+
+    return(TRUE)
+  }
+)
